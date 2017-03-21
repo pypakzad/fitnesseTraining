2d05bd72b6163c587c1cf7ee7aef4a69f1e75e68
Diego Paris <raphaelparisdi@gmail.com>
2017-03-21 15:00:31 -0400

    working on Map

--- WumpusGame/src/game/map/Map.java
+++ WumpusGame/src/game/map/Map.java
@@ -1,36 +1,100 @@
 package game.map;
 
 import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Random;
 
 public class Map {
 	
-	private ArrayList<Cavern> caverns = new ArrayList<Cavern>();
+	private HashMap<Coordinate, Cavern> caverns = new HashMap<Coordinate, Cavern>();
 
 	public Map(int mapsize) {
 		makeCaverns(mapsize);
 	}
 	
+	private class Coordinate {
+		int x;
+		int y;
+		Coordinate(int newx, int newy) {
+			x = newx;
+			y = newy;
+		}
+		Coordinate move(char direction){
+			switch (direction) {
+			case 'N':	northCavern=cavern;
+						northCavern.southCavern = this;
+						break;
+			case 'W':	westCavern=cavern;
+						westCavern.eastCavern = this;
+						break;
+			case 'S':	southCavern=cavern;
+						southCavern.northCavern = this;
+						break;
+			case 'E':	eastCavern=cavern;
+						eastCavern.westCavern = this;
+			}
+			
+		}
+
+	}
+	
 	public class Cavern {
 		Cavern northCavern;
-		public void connectNorthCavern(Cavern cavern) {
-			northCavern = cavern;
+		Cavern eastCavern;
+		Cavern westCavern;
+		Cavern southCavern;
+		private void connectCavern(Cavern cavern, char direction) {
+			switch (direction) {
+			case 'N':	northCavern=cavern;
+						northCavern.southCavern = this;
+						break;
+			case 'W':	westCavern=cavern;
+						westCavern.eastCavern = this;
+						break;
+			case 'S':	southCavern=cavern;
+						southCavern.northCavern = this;
+						break;
+			case 'E':	eastCavern=cavern;
+						eastCavern.westCavern = this;
+			}
 		}
-		public Cavern getNorthCavern() {
-			return northCavern;
+		public Cavern getCavern(char direction) {
+			switch (direction) {
+				case 'N':	return northCavern;
+				case 'W':	return westCavern;
+				case 'S':	return southCavern;
+				case 'E':	return eastCavern;
+			}
+			return null;
 		}
 	}
 
-	public ArrayList<Cavern> getCaverns() {
+	public HashMap<Coordinate, Cavern> getCaverns() {
 		return caverns;
 	}
 
 	private void makeCaverns(int mapsize) {
 		Cavern firstCavern = new Cavern();
-		caverns.add(firstCavern);	
+		caverns.put(new Coordinate(0,0), firstCavern);
+		Random connectionGenerator = new Random(System.currentTimeMillis());
+		int connection = connectionGenerator.nextInt(4)+1;
+		char[] directions = new char[4];
+		directions[0] = 'N';
+		directions[1] = 'W';
+		directions[2] = 'S';
+		directions[3] = 'E';
+		int currentCavern = 0;
+		Coordinate currentCoordinate = new Coordinate(0,0);
 		for (int i=0; i<mapsize; i++) {
 			Cavern cavern = new Cavern();
-			cavern.connectNorthCavern(caverns.get(0));
+			cavern.connectCavern(caverns.get(currentCavern), directions[connection]);
 			caverns.add(cavern);
+			connection--;
+			if (connection<0) {
+				connection = connectionGenerator.nextInt(5);
+				currentCavern++;
+			}
 		}
 	}
 	
