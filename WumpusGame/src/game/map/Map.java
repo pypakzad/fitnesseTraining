607c9cf89fd80066e3ca7f90d5a43833589fe4a0
Diego Paris <raphaelparisdi@gmail.com>
2017-03-21 13:33:06 -0400

    Map and Map test started.

--- WumpusGame/src/game/map/Map.java
+++ WumpusGame/src/game/map/Map.java
@@ -1,5 +1,37 @@
 package game.map;
 
-public class Map {
+import java.util.ArrayList;
 
+public class Map {
+	
+	private ArrayList<Cavern> caverns = new ArrayList<Cavern>();
+
+	public Map(int mapsize) {
+		makeCaverns(mapsize);
+	}
+	
+	public class Cavern {
+		Cavern northCavern;
+		public void connectNorthCavern(Cavern cavern) {
+			northCavern = cavern;
+		}
+		public Cavern getNorthCavern() {
+			return northCavern;
+		}
+	}
+
+	public ArrayList<Cavern> getCaverns() {
+		return caverns;
+	}
+
+	private void makeCaverns(int mapsize) {
+		Cavern firstCavern = new Cavern();
+		caverns.add(firstCavern);	
+		for (int i=0; i<mapsize; i++) {
+			Cavern cavern = new Cavern();
+			cavern.connectNorthCavern(caverns.get(0));
+			caverns.add(cavern);
+		}
+	}
+	
 }
