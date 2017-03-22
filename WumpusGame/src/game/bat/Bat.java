093c0afedb91132a0430f5580d0e1f644f49d18b
parker <mrparker21@gmail.com>
2017-03-22 11:08:26 -0400

    created bat class

---
+++ WumpusGame/src/game/bat/Bat.java
@@ -0,0 +1,12 @@
+package game.bat;
+
+public class Bat {
+	public Bat() {
+
+	}
+
+	public String eventMessage() {
+		return "You have been carried away by bats and dropped in a rondom cavern!";
+	}
+
+}
