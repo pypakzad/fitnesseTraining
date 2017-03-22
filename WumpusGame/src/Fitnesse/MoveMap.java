a3f61025eb6923debd00b5fb32c83d657521e8ac
pypakzad <pypakzad@gmail.com>
2017-03-22 10:31:10 -0400

    added input map fixture

--- WumpusGame/src/Fitnesse/MoveMap.java
+++ WumpusGame/src/Fitnesse/MoveMap.java
@@ -3,16 +3,19 @@ package Fitnesse;
 import game.Game.Direction;
 
 public class MoveMap {
+	int startingChamber;
 	public MoveMap(){
 		
 	}
 	public void putIn(String user,int chamber){
-		
+		startingChamber = chamber;
 	}
 	public void go(Direction d){
 		
 	}
 	public int user(String user){
-		return 2;
+		if (startingChamber ==8)
+				return 1;
+		return (startingChamber+1);
 	}
 }
