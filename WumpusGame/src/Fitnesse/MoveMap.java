b155857658872262f2f7a40c38a383169443a69a
pypakzad <pypakzad@gmail.com>
2017-03-22 09:19:53 -0400

    fixtures added

---
+++ WumpusGame/src/Fitnesse/MoveMap.java
@@ -0,0 +1,18 @@
+package Fitnesse;
+
+import game.Game.Direction;
+
+public class MoveMap {
+	public MoveMap(){
+		
+	}
+	public void putIn(String user,int chamber){
+		
+	}
+	public void go(Direction d){
+		
+	}
+	public int user(String user){
+		return 2;
+	}
+}
